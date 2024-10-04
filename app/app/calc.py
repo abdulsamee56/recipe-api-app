b36c76e0e96f2889b7878ce4a74b779c21bbffad
Abdul Samee <sameeabdul56@gmail.com>
2024-10-04 12:19:55 -0400

    Set up postgres database

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+calculator functions
+"""
+
+def add(x,y):
+    """add x and y retun result"""
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    res = calc.add(5,6)
+
+    self.assertEqual(res,11)
